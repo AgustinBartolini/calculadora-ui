ab9c1e4ce6f462283c8356e4544f36c8253258b4
AgustinBartolini <92292613+AgustinBartolini@users.noreply.github.com>
2024-02-29 14:25:54 +0100

    Add files via upload

---
+++ src/main/java/com/calculadora/CalculadoraUiApplication.java
@@ -0,0 +1,13 @@
+package com.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraUiApplication.class, args);
+	}
+
+}
